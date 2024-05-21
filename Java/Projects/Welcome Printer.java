bcdf4b331abf51a1cff893a4903df4ed2c3ce353
Trung Cao <146856547+Caoskii@users.noreply.github.com>
2024-05-21 17:39:56 -0500

    Project

---
+++ Java/Projects/Welcome Printer.java
@@ -0,0 +1,11 @@
+public class Welcome {
+	public static void main(String[] args) {
+		// Display message Welcome to Java! on the console
+		System.out.println("Welcome to Java!");
+		System.out.println("Welcome to Java!");
+		System.out.println("Welcome to Java!");
+		System.out.println("Welcome to Java!");
+		System.out.println("Welcome to Java!");
+		// Option > Command > Arrow Key Down to duplicate the line
+	}
+}
