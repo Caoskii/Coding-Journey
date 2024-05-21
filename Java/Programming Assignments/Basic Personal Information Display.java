096b9664bfae2cc819c2dcedbcf9ee02cdbd3d4f
Trung Cao <146856547+Caoskii@users.noreply.github.com>
2024-05-20 20:05:00 -0500

    Create Basic Personal Information Display.java

---
+++ Java/Programming Assignments/Basic Personal Information Display.java
@@ -0,0 +1,21 @@
+
+public class Program_02 {
+	public static void main(String[] args) {
+		// Declaration
+		String firstName = "John";
+		System.out.println("First Name: " + firstName);
+		
+		String lastName = "Baker";
+		System.out.println("Last Name: " + lastName);
+		
+		int age = 23;
+		System.out.println("Age: " + age);
+		
+		double hourlyWage = 12.45;
+		System.out.println("Hourly Wage: $" + hourlyWage);
+		
+		char letter = 'M';
+		System.out.println("Gender: " + letter);
+		
+	}
+}
