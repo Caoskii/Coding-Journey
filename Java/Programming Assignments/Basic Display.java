19a976c8bebf7cc9a3f1bccbcb0a9c966fb70c6c
Trung Cao <146856547+Caoskii@users.noreply.github.com>
2024-05-20 19:57:54 -0500

    Assignment

---
+++ Java/Programming Assignments/Basic Display.java
@@ -0,0 +1,8 @@
+// Create a simple program that displays the following:
+public class Program_02 {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java!");
+		System.out.println("Welcome to Computer Science");
+		System.out.println("Programming is so much fun!!!");
+	}
+}
