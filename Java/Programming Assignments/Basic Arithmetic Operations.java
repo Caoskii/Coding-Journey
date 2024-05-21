01a71244769b32cd792720e39cade357cd68c127
Trung Cao <146856547+Caoskii@users.noreply.github.com>
2024-05-20 20:01:30 -0500

    Assignment

---
+++ Java/Programming Assignments/Basic Arithmetic Operations.java
@@ -0,0 +1,10 @@
+// Create a simple program that displays the result of the following arithmetic operations
+public class Program_03 {
+	public static void main(String[] args) {
+		System.out.print("9.5 * 3 + 2 - 4.5 is ");
+		System.out.println(9.5 * 3 + 2 - 4.5);
+		
+		System.out.print("12 + 25 - 10 * 4.5 is ");
+		System.out.println(12 + 25 - 10 * 4.5);
+	}
+}
